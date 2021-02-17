579698d83ab1091bf909ba1cac75ca1375a7f4d6
Roman <blackwanted@yandex.ru>
2021-02-17 03:26:20 +0300

    --Minor

--- t.ts
+++ t.ts
@@ -5,6 +5,18 @@ import { Image, ImageType } from "./src/Image.ts";
 
 const img = await Image.from<BitmapRGB>(`C:/Users/black/Desktop/a/t/mina.bmp`);
 
+const table: { [x: number]: number } = {};
+img.bitmap.channel.red.pixels.map((x) => {
+    x = ((x * 255) / 64) | 0;
+    if (!table[x]) {
+        table[x] = 0;
+    }
+    table[x] += 1;
+    return x / 64;
+});
+
+console.log(table);
+
 /*console.log(img.bitmap.getPixel(0, 0));
 console.log(img.bitmap.getPixel(1, 0));
 console.log(img.bitmap.getPixel(1, 1));
